f75e618ae2117d09d9cc8e692c6644f11d446add
Vincent Hellner <hellner.vincent@gmail.com>
2024-02-14 00:30:49 +0100

    Add file input via command-line argument

--- src/main.cpp
+++ src/main.cpp
@@ -12,69 +12,62 @@
 
 #include "Parser.h"
 
-int main()
+int main(int argc, char* argv[])
 {
 	Lexer lexer;
 	Parser parser;
 	Interpreter interpreter;
 
-	while (true) 
+	std::string input_code;
+
+	if (argc >= 2)
 	{
-		std::string input_code;
-		std::getline(std::cin, input_code);
+		std::ifstream input_file(argv[1]);
 
-		if (input_code.starts_with("file:")) 
+		if (!input_file.is_open())
 		{
-			input_code.erase(std::remove(input_code.begin(), input_code.end(), ' '), input_code.end());
-
-			std::ifstream input_file(input_code.substr(5));
-			
-			if (!input_file.is_open()) 
-			{
-				std::cerr << "Error opening file!" << std::endl;
-				continue;
-			}
-
-			std::ostringstream code_stream;
-			code_stream << input_file.rdbuf();
-			input_code = code_stream.str();
+			std::cout << "Cannot open file: " << argv[1] << std::endl;
+			return -1;
 		}
 
-		auto lexer_res = lexer.tokenize(input_code);
-		if (lexer_res.is_error())
-		{
-			Error err = lexer_res.get_error();
-			std::cout << err.message << " at: "<< err.position << std::endl;
-			continue;
-		}
-
-		const auto& tokens = *lexer_res;
-
-		for (const auto& token : tokens)
-		{
-			token.print();
-			std::cout << std::endl;
-		}
-
-		auto parser_res = parser.parse(tokens);
-
-		if (parser_res.is_error())
-		{
-			for (const auto& err : parser_res.get_error())
-				std::cout << err.message << " at: " << err.position << std::endl;
-			continue;
-		}
-
-		const auto& tree = *parser_res;
-
-		if (tree.size() == 0) continue;
-
-		for (int i = 0; i < tree.size(); ++i)
-		{
-			const auto& res = interpreter.interpret(*tree[i]);
-			if (res.is_error()) 
-				std::cout << res.get_error() << std::endl;
-		}
+		std::ostringstream code_stream;
+		code_stream << input_file.rdbuf();
+		input_code = code_stream.str();
 	}
+	else 
+	{
+		std::cout << "usage: " << argv[0] << " <input file>" << std::endl;
+		return -1;
+	}
+
+	auto lexer_res = lexer.tokenize(input_code);
+	if (lexer_res.is_error())
+	{
+		Error err = lexer_res.get_error();
+		std::cout << err.message << " at: "<< err.position << std::endl;
+		return -1;
+	}
+
+	const auto& tokens = *lexer_res;
+	auto parser_res = parser.parse(tokens);
+
+	if (parser_res.is_error())
+	{
+		for (const auto& err : parser_res.get_error())
+			std::cout << err.message << " at: " << err.position << std::endl;
+		return -1;
+	}
+
+	const auto& tree = *parser_res;
+
+	if (tree.size() == 0) return 0;
+
+	for (int i = 0; i < tree.size(); ++i)
+	{
+		const auto& res = interpreter.interpret(*tree[i]);
+		if (res.is_error()) 
+			std::cout << res.get_error() << std::endl;
+	}
+
 	return 0;
 }
